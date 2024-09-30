3a6659ff8972bd9e8c6f730b8c1bbe124b02b9b4
Baltazarx <144795383+Baltazarx@users.noreply.github.com>
2024-10-01 06:45:42 +0700

    Add files via upload

---
+++ Backend/cek password.py
@@ -0,0 +1,8 @@
+password_benar = "1234"
+input_password = input("Masukkan password: ")
+
+if input_password == password_benar:
+    print("Selamat datang bos!")
+else:
+    print("Password salah, coba lagi!")
+    print("Terimakasih sudah menggunakan aplikasi kami.")
---
+++ Backend/diskon belanja.py
@@ -0,0 +1,7 @@
+total_belanja = float(input("Masukkan total belanja Anda: "))
+
+if total_belanja > 100000:
+    print("Selamat, Anda dapat hadiah!")
+else:
+    print("Terimakasih sudah berbelanja.")
+    
